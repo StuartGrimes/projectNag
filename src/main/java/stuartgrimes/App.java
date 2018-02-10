9e6ffe8671b6ad12534e360bfa79098e457b3d18
StuartGrimes <17004808+StuartGrimes@users.noreply.github.com>
2018-02-10 17:56:04 +0000

    App modified from default

--- src/main/java/stuartgrimes/App.java
+++ src/main/java/stuartgrimes/App.java
@@ -1,13 +1,51 @@
 package stuartgrimes;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private Scanner someinput;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //see commanline args beeing sent in
+        seeCommandLineInput( args);
+        //to instantiate an instance of App
+        actionCommandLineInput(args);
+    }
+
+    //Constructor
+    public App(){
+        this.someinput = new Scanner(System.in);
+        System.out.println("\n...soon we will do something here.");
+        this.someinput.nextLine();
+        //close program without error
+        System.exit(0);
+
+    }
+
+    /**
+     *
+     */
+    public static void actionCommandLineInput(String[] args){
+        App myApp = new App();
+    }
+    /**
+     * View the arguments presented at the command line
+     * @param args
+     */
+    private static void seeCommandLineInput(String[] args){
+        if (args.length == 0){
+            System.out.println("No command line args sent in...");
+        }else{
+            //display the command line args entered.
+            for (int i =0; i<args.length; i++){
+                System.out.println(args[i]);
+            }
+        }
     }
 }
